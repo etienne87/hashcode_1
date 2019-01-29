4e118124a670d2a26f4923b08c099ffbee80218c
Laurent George <laurent.f.george@gmail.com>
2019-01-29 21:42:15 +0100

    boom

--- solution_laurent.py
+++ solution_laurent.py
@@ -19,43 +19,44 @@ while qu'un cache a de la place:
 import numpy as np
 import time
 from numba.decorators import jit
+from input_func import input_func
 
-example_case = {}
-n_ep = 1000
-n_video = 10000
-n_cache = 1000
+import os
+import sys
 
 
-ep_video_requests = np.random.random([n_ep, n_video]).astype(np.int)
-gain_latence = np.random.random([n_ep, n_cache]).astype(np.int)
-cache = np.zeros([n_cache])
-output = np.zeros([n_cache, n_video], dtype=np.bool)
+file_path = "./streaming/me_at_the_zoo.in"
+
+video_size, cache, gain_latence, ep_video_requests = input_func(file_path)
+n_cache = gain_latence.shape[1]
+cache_size = np.ones(n_cache) * cache
+#ep_video_requests = np.random.random([n_ep, n_video]).astype(np.int)
+#gain_latence = np.random.random([n_ep, n_cache]).astype(np.int)
+#cache = np.zeros([n_cache])
+#output = np.zeros([n_cache, n_video], dtype=np.bool)
 
 def get_best_video(cache_idx):
-    """
-    best video for this cache
-    :param cache_idx:
-    :return:
-    """
-    #best_video = -1
-    #best_gain = 0
-    #for video_idx in range(ep_video_requests.shape[1]):
-    #    video_gain = 0
-    #    for ep_idx in range(gain_latence.shape[0]):
-    #        #print(f"ep is {ep_idx}")
-    #        cur_gain = gain_latence[ep_idx, cache_idx] * ep_video_requests[ep_idx, video_idx]
-    #        video_gain += cur_gain
-    #    #dot_res = np.dot(ep_video_requests[:, video_idx], gain_latence[:, cache_idx])
-    #    #print(f"dot_res is {dot_res}, video_gain {video_gain}")
-    #    best_gain, best_video = np.max([best_gain, best_video], [video_gain, video_idx])
-    #return best_video
-
-    np.argmin()
+    pass
 
 
+gain_score = gain_latence.T.dot(ep_video_requests)  # shape =  VxC
+# init:
+for video_idx in range(video_size.shape[0]):
+    # removing videos that don't fit
+    for cache_idx in range(cache_size.shape[0]):
+        if cache_size[cache_idx] - video_size[video_idx] < 0:
+            print(f"UPDATING CACHE {cache_idx} for video {video_idx}, cache_size is {cache_size[cache_idx]} whereas video size is video_size[video_idx")
+            gain_score[cache_idx, video_idx] = 0
 
-gain_score = gain_latence.T.dot(ep_video_requests)
+#def update_gain(video_idx, cache_idx):
+#    for video in range(video_size.shape):
+
+
 print("gain_score shape is {}".format(gain_score))
+print(gain_score)
+
+exit(0)
+#n_cache
 
 
 
