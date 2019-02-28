c26d64a0da214a83649eb15ef991a717b7e07ffe
Laurent George <laurent.f.george@gmail.com>
2019-02-28 19:58:48 +0100

    score transition bis

--- compute_score.py
+++ compute_score.py
@@ -27,12 +27,35 @@ def read_submission(path):
 
     return slideshow
 
-def compute_score_transition(tags1, tasg2):
-    return 0
+
+def compute_score_transition(s1: list, s2: list):
+    """
+    compute score transition
+
+    :param s1:
+    :param s2:
+    :return:
+
+    assert(compute_score_transition([1,2,3], [3,5,6]) == 1)
+    assert(compute_score_transition([1,2], [6]) == 0)
+    assert(compute_score_transition([1,2,3, 4], [3,4,5,6]) == 2)
+    """
+    s1 = set(s1)
+    s2 = set(s2)
+    print(f"S1 {s1}")
+    print(f"S2 {s1}")
+    s_intersection = len(s1.intersection(s2))
+    s_in_s1_not_in_s2 = len([tag for tag in s1 if tag not in s2])
+    s_in_s2_not_in_s1 = len([tag for tag in s2 if tag not in s1])
+
+    score = min([s_intersection,s_in_s1_not_in_s2, s_in_s2_not_in_s1])
+    print(f"Score is {score}")
+    return score
 
 
 def compute_score(path):
     slideshow = read_submission(path)
+    print(slideshow)
 
     score = 0
 
@@ -44,8 +67,8 @@ def compute_score(path):
 
 if __name__ == '__main__':
     path = 'submit_bidon.txt'
-
-    submission = read_submission(path)
+    score = compute_score(path)
+    print(score)
 
 
 
