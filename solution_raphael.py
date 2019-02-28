2532a8116044b0e7c2154b4244408760bedda7ce
raphael <raphael.roseandrieux@gmail.com>
2019-02-28 19:38:57 +0100

    raph

--- solution_raphael.py
+++ solution_raphael.py
@@ -4,13 +4,71 @@ import sys
 from input_func import input_func
 
 
-file_path = "./streaming/me_at_the_zoo.in"
 
 
-video_size, cache_size, gain_per_ep_per_cach, nb_request_per_ep_per_video = input_func(file_path)
+
+def get_horiz_from_vert(list_vert):
+
+    half_length = len(list_vert)/2
+    list_horiz_bonus = []
+    for i in half_length:
+        list_horiz_bonus += (str(list_vert[2*i][0])+", "+str(list_vert[2*i+1]), list(set(list_vert[2*i][1]+list_vert[2*i+1][1])))
+    return list_horiz_bonus
 
 
-print(video_size, cache_size, gain_per_ep_per_cach, nb_request_per_ep_per_video)
+def get_first_frame(list_all):
+    idx = np.random.randint(0, len(list_all))
+
+    return idx
+
+def add_image_to_slide(idx, result_list, array_availability, list_all):
+
+    result_list += [list_all[id_first][0]]
+    array_availability[idx] = 0
+    return 0
+
+
+
+
+def find_next_image(last_image, list_all, array_availability):
+
+
+
+
+def main(list_horiz, list_vert):
+
+    list_horiz_bonus = get_horiz_from_vert(list_vert)
+
+
+    result_list = []
+
+    list_all = list_horiz+list_horiz_bonus
+    array_availability = np.ones(len(list_all))
+
+
+    id_first = get_first_frame(list_all)
+
+    add_image_to_slide(id_first, result_list, array_availability, list_all)
+
+    last_image = list_all[id_first]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
