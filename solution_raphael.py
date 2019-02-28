ad06c9bc377e1a2947c728365576057a23c4d756
raphael <raphael.roseandrieux@gmail.com>
2019-02-28 20:44:43 +0100

    vlub

--- solution_raphael.py
+++ solution_raphael.py
@@ -2,8 +2,8 @@ import numpy as np
 import os
 import sys
 from input_func import input_func
-from compute_score import compute_score_transition
-
+from compute_score import compute_score_transition, compute_score
+from tqdm import tqdm
 
 
 
@@ -32,17 +32,25 @@ def add_image_to_slide(idx, result_list, array_availability, list_all):
 
 def find_next_image(last_image, list_all, array_availability):
 
-    MIN_SCORE = 1
+    MIN_SCORE = 2
 
     list_available = np.where(array_availability==1)[0]
+    max_score = 0
+    best_id = "bla"
     for idx in list_available:
         score = compute_score_transition(last_image[1], list_all[idx][1])
-
         if score >= MIN_SCORE:
             return idx
+        if score >max_score:
+            max_score=score
+            best_id=idx
+
+
+    if max_score>0:
+        return best_id
 
     idx = list_available[np.random.randint(len(list_available))]
-
+    print("chosing random image")
     return idx
 
 
@@ -67,17 +75,29 @@ def formatting_input(list_imag,list_id):
 
 def main():
 
-    file_path = "data/a_example.txt"
+
+
+   # file_name = "c_memorable_moments"
+   # file_name = "b_lovely_landscapes"
+    file_name = "d_pet_pictures"
+   # file_name = "e_shiny_selfies"
+
+
+
+    file_path = "data/"+file_name+".txt"
+
 
     horizontals, horizontal_ids, verticals, vertical_ids, dic, all_tags = input_func(file_path)
 
 
+    print("number of horizontals :", len(horizontals))
+    print("number of verticals :", len(verticals))
 
     list_horiz = formatting_input(horizontals, horizontal_ids)
     list_vert = formatting_input(verticals, vertical_ids)
 
 
-    print("debug",list_horiz)
+    print("parsing done")
 
     list_horiz_bonus = get_horiz_from_vert(list_vert)
 
@@ -96,16 +116,26 @@ def main():
     last_image = list_all[id_first]
 
 
-    for i in range(min(len(list_all)-1,100)):
+    for i in tqdm(range(len(list_all)-1)):
         idx_next = find_next_image(last_image, list_all, array_availability)
         add_image_to_slide(idx_next, result_list, array_availability, list_all)
         last_image = list_all[idx_next]
 
-        print("one_more_image")
-        print(result_list)
+        if i%1000==0:
+            out_path_debug = "raphael2_"+file_name+"_debug_{}.out".format(i)
+            write_output(result_list, out_path_debug)
+            print(compute_score(file_path, out_path_debug))
 
-    write_output(result_list, "raphael_test.out")
 
+        #print("one_more_image")
+        #print(result_list)
+
+    out_path = "raphael2_"+file_name+".out"
+    write_output(result_list, out_path)
+
+
+    print("result for file =",file_path)
+    print(compute_score(file_path, out_path))
 
 
 
