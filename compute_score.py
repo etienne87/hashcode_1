a4c35b95498b92afeb1258a9651b875593c49e05
Laurent George <laurent.f.george@gmail.com>
2019-02-28 20:01:14 +0100

    score fix

--- compute_score.py
+++ compute_score.py
@@ -42,14 +42,11 @@ def compute_score_transition(s1: list, s2: list):
     """
     s1 = set(s1)
     s2 = set(s2)
-    print(f"S1 {s1}")
-    print(f"S2 {s1}")
     s_intersection = len(s1.intersection(s2))
     s_in_s1_not_in_s2 = len([tag for tag in s1 if tag not in s2])
     s_in_s2_not_in_s1 = len([tag for tag in s2 if tag not in s1])
 
     score = min([s_intersection,s_in_s1_not_in_s2, s_in_s2_not_in_s1])
-    print(f"Score is {score}")
     return score
 
 
